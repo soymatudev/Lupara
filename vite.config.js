bdc383c5a6f5845b328ae73030d337dd7936ae35
SoyMatudev <juanjesusgarciamaturana@gmail.com>
2025-12-21 12:35:49 -0600

    Chore: vercel

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,23 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   root: 'frontend',
   build: {
     outDir: '../dist',
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'frontend/index.html'),
+        auth: resolve(__dirname, 'frontend/views/auth/index.html'),
+        forgot_password: resolve(__dirname, 'frontend/views/forgot-password/index.html'),
+        health: resolve(__dirname, 'frontend/views/health/index.html'),
+        home: resolve(__dirname, 'frontend/views/home/index.html'),
+        mis_reservas: resolve(__dirname, 'frontend/views/profile/mis-reservas.html'),
+        register: resolve(__dirname, 'frontend/views/register/index.html'),
+        details: resolve(__dirname, 'frontend/views/reserva/details.html'),
+      }
+    }
   },
   server: {
     proxy: {
